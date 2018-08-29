0f3351abb4a852237c3e771e452b6a06405e7b86
Adebayo Oluwadamilola <goshensoftinc@gmail.com>
2018-08-29 22:45:43 +0100

    initail commit

---
+++ betasms-one-pager-index.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+  Plugin Name: The One Pager Plugin
+  Description: Plugin for doing sending SMS, buying DATA, pay bills etc
+  Version: 1
+  Author: Betasms
+  Author URI: http://GoshenSoftInc.com
+  Plugin URL: https://github.com/OgaBoss/one-pager-wp-plugin
+ */
+add_action('admin_menu', 'at_alphansotech_menu');
+add_action('wp_enqueue_scripts', 'add_up_styles');
+
+function at_alphansotech_menu() {
+    add_menu_page('employee_list', //page title
+        'Employee Listing', //menu title
+        'manage_options', //capabilities
+        'Employee Listing', //menu slug
+        'employee_list' //function
+    );
+}
+
+function add_up_styles() {
+    wp_register_style( 'normalize',  plugins_url('css/normalize.css', __FILE__));
+    wp_register_style( 'default',  plugins_url('css/default.css', __FILE__));
+    wp_register_style( 'tabs',  plugins_url('css/tabs.css', __FILE__));
+    wp_register_style( 'tabstyles',  plugins_url('css/tabstyles.css', __FILE__));
+    wp_register_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css', array(), '4.2.0' );
+
+    wp_enqueue_style( 'fontawesome' );
+    wp_enqueue_style('normalize');
+    wp_enqueue_style('default');
+    wp_enqueue_style('tabs');
+    wp_enqueue_style('tabstyles');
+
+}
+
+function add_up_scripts() {}
+
+
+// returns the root directory path of particular plugin
+define('ROOTDIR', plugin_dir_path(__FILE__));
+require_once(ROOTDIR . '/partials/iframe-one-pager.php');
---
+++ partials/iframe-one-pager.php
@@ -0,0 +1,42 @@
+<?php
+function i_frame_one_pager() {
+?>
+    <section>
+        <div class="tabs tabs-style-flip">
+            <nav>
+                <ul>
+                    <li><a href="#section-flip-1"><span><i class="far fa-comment-alt"></i> SMS</span></a></li>
+                    <li><a href="#section-flip-4"><span><i class="fas fa-mobile"></i> Data</span></a></li>
+                    <li><a href="#section-flip-2"><span><i class="far fa-credit-card"></i> Bill</span></a></li>
+                    <li><a href="#section-flip-3"><span><i class="fas fa-share-square"></i> Transfer</span></a></li>
+                    <li><a href="#section-flip-5"><span><i class="fas fa-cogs"></i> Settings</span></a></li>
+                </ul>
+            </nav>
+            <div class="content-wrap">
+                <section id="section-flip-1">
+                    <iframe src="http://login.betasms.com.ng/ordersms/?site=7e22ec46-93f1-11e8-9354-beee55e65685" height="800" width="400"></iframe>
+                </section>
+                <section id="section-flip-2"><p>coming soon</p></section>
+                <section id="section-flip-3"><p>coming soon</p></section>
+                <section id="section-flip-4"><p>coming soon</p></section>
+                <section id="section-flip-5"><p>coming soon</p></section>
+            </div><!-- /content -->
+        </div><!-- /tabs -->
+    </section>
+    <script src=<?= plugins_url('betasms-one-pager/js/modernizr.custom.js')?>></script>
+    <script src=<?= plugins_url('betasms-one-pager/js/cbpFWTabs.js')?>></script>
+    <script>
+        (function() {
+
+            [].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
+                console.log(el);
+                new CBPFWTabs( el );
+            });
+
+        })();
+    </script>
+    <?php
+}
+
+add_shortcode('one-pager', 'i_frame_one_pager');
+?>
